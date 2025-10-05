a30f2fb39ce4b10b0bc45f3ad19cb42c9674ed7b
Skye J <skye@skyenet.online>
2025-10-05 15:31:40 -0400

    feat!: switch this to just be a nixos module

--- default.nix
+++ default.nix
@@ -1,9 +1,77 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
-  pkgs = import nixpkgs {
-    config = {};
-    overlays = [];
+  cfg = config.programs.iterm2-shell-integration;
+  enabledOption =
+    x:
+    lib.mkEnableOption x
+    // {
+      default = true;
+      example = false;
+    };
+  format = pkgs.formats.toml { };
+in
+{
+  options.programs.iterm2-shell-integration = {
+
+    enable = lib.mkEnableOption ''
+      direnv integration. Takes care of both installation and
+      setting up the sourcing of the shell. Additionally enables nix-direnv
+      integration. Note that you need to logout and login for this change to apply
+    '';
+
+    package = lib.mkPackageOption pkgs "iterm2-shell-integration" { };
+
+    enableBashIntegration = enabledOption ''
+      Bash integration
+    '';
+    enableZshIntegration = enabledOption ''
+      Zsh integration
+    '';
+    enableFishIntegration = enabledOption ''
+      Fish integration
+    '';
+
+    loadInNixShell = enabledOption ''
+      loading direnv in `nix-shell` `nix shell` or `nix develop`
+    '';
+
   };
-in {
-  it2shell = pkgs.callPackage ./it2shell.nix {};
+
+  config = lib.mkIf cfg.enable {
+    programs = {
+      zsh.interactiveShellInit = lib.mkIf cfg.enableZshIntegration ''
+        if ${lib.boolToString cfg.loadInNixShell} || printenv PATH | grep -vqc '/nix/store'; then
+          source ${cfg.iterm2-shell-integration.package}/share/iterm2-shell-integration/iterm2_shell_integration.zsh
+        fi
+      '';
+
+      #$NIX_GCROOT for "nix develop" https://github.com/NixOS/nix/blob/6db66ebfc55769edd0c6bc70fcbd76246d4d26e0/src/nix/develop.cc#L530
+      #$IN_NIX_SHELL for "nix-shell"
+      bash.interactiveShellInit = lib.mkIf cfg.enableBashIntegration ''
+        if ${lib.boolToString cfg.loadInNixShell} || [ -z "$IN_NIX_SHELL$NIX_GCROOT$(printenv PATH | grep '/nix/store')" ] ; then
+          source ${cfg.iterm2-shell-integration.package}/share/iterm2-shell-integration/iterm2_shell_integration.bash
+        fi
+      '';
+
+      fish.interactiveShellInit = lib.mkIf cfg.enableFishIntegration ''
+        if ${lib.boolToString cfg.loadInNixShell}; or printenv PATH | grep -vqc '/nix/store';
+          source ${cfg.iterm2-shell-integration.package}/share/iterm2-shell-integration/iterm2_shell_integration.fish
+        end
+      '';
+
+    };
+
+    environment = {
+      systemPackages = [
+        cfg.package
+      ];
+
+      variables.DIRENV_CONFIG = "/etc/direnv";
+    };
+  };
 }
--- iterm2_shell_integration_selector.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if $SHELL == "zsh"; then
-    source iterm2_shell_integration.zsh
-elif $SHELL == "bash"; then
-    source iterm2_shell_integration.bash
-elif $SHELL == "fish"; then
-    source iterm2_shell_integration.fish
-fi
