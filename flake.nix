4cfe580de5a5959f09f905886bc85ab447461bae
Skye J <skye@skyenet.online>
2026-01-22 12:37:16 -0500

    feat(flake): Added a nix flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "NixOS module for iTerm2 shell integration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # Stable NixOS users can override this input in their flake:
+    # inputs.nixos-iterm2-shell-integration.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    {
+      # NixOS module for all systems
+      nixosModules.default = import ./default.nix;
+      nixosModules.iterm2-shell-integration = import ./default.nix;
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        # Development shell
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nil
+            alejandra
+          ];
+        };
+      }
+    );
+}
